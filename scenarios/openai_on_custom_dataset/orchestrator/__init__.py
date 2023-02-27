1c6473f27bd4c3755da1045f80c122f223fb3a45
james-tn <james.nguyen@microsoft.com>
2023-02-27 10:26:33 -0800

    update openai on custom dataset scenario

--- scenarios/openai_on_custom_dataset/orchestrator/__init__.py
+++ scenarios/openai_on_custom_dataset/orchestrator/__init__.py
@@ -14,9 +14,6 @@ from azure.search.documents import SearchClient
 GPT_ENGINE = os.getenv("GPT_ENGINE")
 PROMPT_URL = os.getenv("PROMPT_URL")
 PROMPT_KEY = os.getenv("PROMPT_KEY")
-CUSTOM_EMB_URL = os.getenv("CUSTOM_EMB_URL")
-CUSTOM_EMB_KEY = os.getenv("CUSTOM_EMB_KEY")
-
 openai.api_type = "azure"
 openai.api_key = os.getenv("OPENAI_API_KEY")  # SET YOUR OWN API KEY HERE
 openai.api_base = os.getenv("OPENAI_RESOURCE_ENDPOINT")  # SET YOUR RESOURCE ENDPOINT
@@ -55,62 +52,6 @@ def azcognitive_score(user_query, topk):
         i+=1
     return f"Answer this question \"{user_query}\" using only the following information  \n <context> {document} </context>"
 
-def tprompt_score(prompt, topk):
-    data = {
-    "inputs": [prompt],
-    "topK": topk,
-    }
-
-    url = PROMPT_URL
-    # Replace this with the primary/secondary key or AMLToken for the endpoint
-    api_key = PROMPT_KEY
-    body = str.encode(json.dumps(data))
-
-    # The azureml-model-deployment header will force the request to go to a specific deployment.
-    # Remove this header to have the request observe the endpoint traffic rules
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'blue' }
-
-    req = urllib.request.Request(url, body, headers)
-
-    try:
-        response = urllib.request.urlopen(req)
-
-        
-        result = json.loads(response.read())
-        return result['prompts'][0]['context']
-
-    except urllib.error.HTTPError as error:
-        return("The request failed with status code: " + str(error.code))
-
-def custom_emb_score(prompt, topk):
-    data = {
-    "prompt": prompt,
-    "topk": topk
-    }
-
-    url = CUSTOM_EMB_URL
-    # Replace this with the primary/secondary key or AMLToken for the endpoint
-    api_key = CUSTOM_EMB_KEY
-    body = str.encode(json.dumps(data))
-
-    # The azureml-model-deployment header will force the request to go to a specific deployment.
-    # Remove this header to have the request observe the endpoint traffic rules
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'green' }
-
-    req = urllib.request.Request(url, body, headers)
-
-    try:
-        response = urllib.request.urlopen(req)
-
-        
-        result = json.loads(response.read())
-        result = f"Answer this question \"{prompt}\" using only the following information  \n <context> {result} </context>"
-        return result
-
-    except urllib.error.HTTPError as error:
-        return("The request failed with status code: " + str(error.code))
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -125,13 +66,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             prompt = req_body.get('prompt')
-    if search_engine == "custom embedding":
-        gpt_prompt = custom_emb_score(prompt, topk)
-    elif search_engine=='azure semantic search':
-        gpt_prompt = azcognitive_score(prompt,topk)
-    else:
-
-        gpt_prompt = tprompt_score(prompt,topk)
+    gpt_prompt = azcognitive_score(prompt,topk)
     print("gpt_prompt ",gpt_prompt )
     result = run_openai(gpt_prompt)
 
