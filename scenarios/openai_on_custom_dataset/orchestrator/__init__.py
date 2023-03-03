1b8ef732a5a1b234de9742f1560697eeb24f96d0
Nicole Serafino <74378487+nicoleserafino@users.noreply.github.com>
2023-03-03 11:09:33 -0600

    data and template

--- scenarios/openai_on_custom_dataset/orchestrator/__init__.py
+++ scenarios/openai_on_custom_dataset/orchestrator/__init__.py
@@ -72,3 +72,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
 
     return func.HttpResponse(json.dumps({"result":result, "gpt_prompt":gpt_prompt}))
+
+
+@app.function_name(name="BlobTrigger1")
+@app.blob_trigger(arg_name="myblob", path="samples-workitems/{name}",
+                  connection="")
+def test_function(myblob: func.InputStream):
+   logging.info(f"Python blob trigger function processed blob \n"
+                f"Name: {myblob.name}\n"
+                f"Blob Size: {myblob.length} bytes")
