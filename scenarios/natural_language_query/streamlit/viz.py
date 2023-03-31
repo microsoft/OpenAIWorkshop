ee10cb5b02bca242ba6260d05d3900ae6de568a6
james-tn <james.nguyen@microsoft.com>
2023-03-31 16:49:42 -0700

    add visualization

---
+++ scenarios/natural_language_query/streamlit/viz.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import random
+import pandas as pd
+from query_viz import AnalyzeGPT
+st.title('Data Analysis Assistant')
+question = st.text_area("Ask me a  question in sales")
+if st.button("Submit"):  
+    # Call the execute_query function with the user's question  
+    analyzer = AnalyzeGPT()
+    analyzer.run(question, st)
+
+
+
