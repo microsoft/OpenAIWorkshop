6d8ec5a0e30907b30e05eca288ff13cd79ec86d3
james-tn <james.nguyen@microsoft.com>
2023-04-21 11:24:48 -0700

    support SQL server fix

--- scenarios/natural_language_query/streamlit/analyze_v1.py
+++ scenarios/natural_language_query/streamlit/analyze_v1.py
@@ -98,16 +98,22 @@ class ChatGPT_Handler: #designed for chatcompletion API
             if len(text_input)==0:
                 return output
             for pattern in self.extract_patterns: 
+                sql_query=""
+                sql_result = re.findall(pattern[1], text_input, re.DOTALL)
 
-                if "Python" in pattern[1]:
-                    result = re.findall(pattern[1], text_input, re.DOTALL)
-                    if len(result)>0:
-                        output[pattern[0]]= result[0]
+                if len(sql_result)>0:
+                    sql_query=sql_result[0]
+                    output[pattern[0]]= sql_query
                 else:
+                    return output
+                text_before = text_input.split(sql_query)[0].strip("\n").strip("```sql").strip("\n")
 
-                    result = re.search(pattern[1], text_input,re.DOTALL)  
-                    if result:  
-                        output[result.group(1)]= result.group(2)
+                if text_before is not None and len(text_before)>0:
+                    output["text_before"]=text_before
+                text_after =text_input.split(sql_query)[1].strip("\n").strip("```")
+                if text_after is not None and len(text_after)>0:
+                    output["text_after"]=text_after
+                # print("extracted output ", output)
 
             return output
 class SQL_Query(ChatGPT_Handler):
@@ -131,12 +137,7 @@ class SQL_Query(ChatGPT_Handler):
         else:  
             connecting_string = f"Driver={{ODBC Driver 17 for SQL Server}};Server=tcp:{self.dbserver},1433;Database={self.database};Uid={self.db_user};Pwd={self.db_password}"
             params = parse.quote_plus(connecting_string)
-
             engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
-
-
-        
-        #logging.info(conn)
 
         result = pd.read_sql_query(query, engine)
         result = result.infer_objects()
@@ -160,17 +161,12 @@ class AnalyzeGPT(ChatGPT_Handler):
 
         
         table_schema = get_table_schema(sql_query_tool,sql_engine)
-        # print("table_schema: \n", table_schema)
-        # if 'conversation_history' not in st.session_state: #first time conversation
         system_message = f"""
         <<data_sources>>
         {table_schema}
         {system_message.format(sql_engine=sql_engine)}
         {few_shot_examples}
         """
-        #     self.conversation_history =  [{"role": "system", "content": system_message}]
-        # else:
-        #     self.conversation_history =  st.session_state['conversation_history']
         self.conversation_history =  [{"role": "system", "content": system_message}]
         self.st = st
         self.content_extractor = content_extractor
@@ -185,7 +181,7 @@ class AnalyzeGPT(ChatGPT_Handler):
         n=0
         try:
             llm_output = self._call_llm(self.conversation_history, stop)
-            # print(llm_output)
+            # print("llm_output \n", llm_output)
         except Exception as e:
             time.sleep(8) #sleep for 8 seconds
             while n<5:
@@ -199,107 +195,64 @@ class AnalyzeGPT(ChatGPT_Handler):
 
             llm_output = "OPENAI_ERROR"     
              
-    
-        # print("llm_output: ", llm_output)
         output = self.content_extractor.extract_output(llm_output)
+        if len(output)==0: #wrong output format
+            llm_output = "WRONG_OUTPUT_FORMAT"
+
+
             
 
         return llm_output,output
 
     def run(self, question: str, show_code,st) -> any:
-        import numpy as np
-        import plotly.express as px
-        import plotly.graph_objs as go
-        import pandas as pd
 
         st.write(f"Question: {question}")
-        # if "init" not in self.st.session_state.keys():
-            
-        #     self.st.session_state['init']= True
-
         def execute_sql(query):
             return self.sql_query_tool.execute_sql_query(query)
-        observation=None
-        def show(data):
-            if type(data) is Figure:
-                st.plotly_chart(data)
-            else:
-                st.write(data)
-            i=0
-            for key in self.st.session_state.keys():
-                if "show" in key:
-                    i +=1
-                self.st.session_state[f'show{i}']=data 
-                if type(data) is not Figure:
-                    self.st.session_state[f'observation: show_to_user{i}']=data
-        def observe(name, data):
-            try:
-                data = data[:10] # limit the print out observation to 15 rows
-            except:
-                pass
-            self.st.session_state[f'observation:{name}']=data
-
         max_steps = 15
         count =1
 
         finish = False
         new_input= f"Question: {question}"
-        # if self.st.session_state['init']:
-        #     new_input= f"Question: {question}"
-        # else:
-        #     new_input=self.st.session_state['history'] +f"\nQuestion: {question}"
-        while not finish:
+        while count<= max_steps:
 
             llm_output,next_steps = self.get_next_steps(new_input, stop=["Observation:", f"Thought {count+1}"])
             if llm_output=='OPENAI_ERROR':
                 st.write("Error Calling Azure Open AI, probably due to max service limit, please try again")
                 break
+            elif llm_output=='WRONG_OUTPUT_FORMAT': #just have open AI try again till the right output comes
+                count +=1
+                continue
+            output =None
+            error= False
+
             new_input += f"\n{llm_output}"
             for key, value in next_steps.items():
                 new_input += f"\n{value}"
                 
-                if "ACTION" in key.upper():
+                if "SQL" in key.upper():
                     if show_code:
-                        st.write(key)
+                        st.write("SQL Code")
                         st.code(value)
-                    observations =[]
-                    serialized_obs=[]
                     try:
-                        # if "print(" in value:
-                        #     raise Exception("You must not use print() statement, instead use st.write() to write to end user or observe(name, data) to view data yourself. Please regenerate the code")
-                        exec(value, locals())
-                        for key in self.st.session_state.keys():
-                            if "observation:" in key:
-                                observation=self.st.session_state[key]
-                                observations.append((key.split(":")[1],observation))
-                                if type(observation) is pd:
-                                    # serialized_obs.append((key.split(":")[1],observation.to_json(orient='records', date_format='iso')))
-                                    serialized_obs.append((key.split(":")[1],observation.to_string()))
-
-                                elif type(observation) is not Figure:
-                                    serialized_obs.append({key.split(":")[1]:str(observation)})
-                                del self.st.session_state[key]
+                        output = execute_sql(value)
                     except Exception as e:
-                        observations.append(("Error:",str(e)))
-                        serialized_obs.append({"Encounter following error, can you try again?\n:":str(e)})
                         
-                    for observation in observations:
-                        st.write(observation[0])
-                        st.write(observation[1])
-
-                    obs = f"\nObservation on the first 10 rows of data: {serialized_obs}"
-                    new_input += obs
+                        new_input +="Encounter following error, can you try again?\n"+str(e)
+                        error=str(e)
                 else:
-                    st.write(key)
-                    st.write(value)
-                if "Answer" in key:
-                    print("Answer is given, finish")
-                    finish= True
+                    if show_code:
+                        st.write(value)
+
+            if output is not None:
+                st.write(output)
+                break
+            if error:
+                st.write(error)
 
             count +=1
             if count>= max_steps:
-                print("Exceeding threshold, finish")
-                break
+                st.write("Cannot handle the question, please change the question and try again")
 
 
 
