7184085cb3404f98d7c9cfa8f00a87fb8f3c0bed
james-tn <james.nguyen@microsoft.com>
2023-08-02 14:18:51 -0700

    add multi agent

--- scenarios/incubations/copilot/multi-agent/copilot.py
+++ scenarios/incubations/copilot/multi-agent/copilot.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
-from utils import Smart_Agent, HR_PERSONA, HR_AVAILABLE_FUNCTIONS, HR_FUNCTIONS_SPEC
+from utils import *
 import concurrent.futures
 import time
 import random
@@ -10,8 +10,9 @@ import openai
 import os
 from pathlib import Path  
 import json
-hr_agent = Smart_Agent(persona=HR_PERSONA,functions_list=HR_AVAILABLE_FUNCTIONS, functions_spec=HR_FUNCTIONS_SPEC, init_message="Hi there, this is Lucy, HR specialist helping with answering questions about HR & Payroll and handle personal information updates, may I have your name and employee ID?")
-
+generic_agent = Smart_Agent(persona=GENERALIST_PERSONA, name="Jenny",functions_spec=GENERAL_FUNCTIONS_SPEC, functions_list= GENERAL_AVAILABLE_FUNCTIONS, init_message="Hi there, this is Jenny, your general support assistant, how can I help you?")
+it_agent = Smart_Agent(persona=IT_PERSONA,name="Paul", functions_list=IT_AVAILABLE_FUNCTIONS, functions_spec=IT_FUNCTIONS_SPEC)
+hr_agent = Smart_Agent(persona=HR_PERSONA,name="Lucy",functions_list=HR_AVAILABLE_FUNCTIONS, functions_spec=HR_FUNCTIONS_SPEC, init_message="Hi there, this is Lucy, HR specialist helping with answering questions about HR & Payroll and handle personal information updates, may I have your name and employee ID?")
 st.set_page_config(layout="wide",page_title="Enterprise Copilot- A demo of Copilot application using GPT")
 styl = f"""
 <style>
@@ -27,9 +28,12 @@ st.markdown(styl, unsafe_allow_html=True)
 MAX_HIST= 5
 # Sidebar contents
 with st.sidebar:
-    st.title('HR/Payroll Copilot')
+    st.title('Copilot')
     st.markdown('''
-    This is a demo of Copilot Concept for HR/Payroll. The Copilot helps employees answer questions and update personal information.
+    This is a demo of Multi-Agent Copilot Concept. The Copilot helps employees answer questions and update information.
+    There are 3 agents in the Copilot: HR, IT and Generalist. Each agent has a different persona and skillset.
+    Depending on the needs of the user, the Copilot will assign the right agent to answer the question.
+    1. For HR Copilot, the agent will answer questions about HR and Payroll and update personal information.
 
     Copilot will first validate the identity of the employee before answering any questions or updating any information.
     Use ids such as 1234 or 5678 to test the demo.
@@ -37,18 +41,20 @@ with st.sidebar:
     Example questions to ask:
     - When do I receive W2 form?
     - What are deducted from my paycheck?    
-    
+    When do I receive W2 form?When do I receive W2 form?
+                
     These questions are answered by the Copilot by searching a knowledge base and providing the answer.
                 
     Copilot also can help update information. 
     - For address update, the Copilot will update the information in the system. 
     - For other information update requests, the Copilot will log a ticket to the HR team to update the information.
-    
+    2. For IT copilot, it helps answer questions about IT
+    3. Generalist copilot helps answer general questions such as company policies, benefits, etc.When do I receive W2 form?
+                
     ''')
     add_vertical_space(5)
     st.write('Created by James N')
     if st.button('Clear Chat'):
-
         if 'history' in st.session_state:
             st.session_state['history'] = []
 
@@ -56,6 +62,8 @@ with st.sidebar:
         st.session_state['history'] = []
     if 'input' not in st.session_state:
         st.session_state['input'] = ""
+    if 'starting_agent_name' not in st.session_state:
+        st.session_state['starting_agent_name'] = "Jenny"
 
 
 
@@ -75,7 +83,8 @@ user_input = get_text()
 
 ## Conditional display of AI generated responses as a function of user provided prompts
 history = st.session_state['history']
-      
+agent_runner = Agent_Runner(starting_agent_name=st.session_state['starting_agent_name'], agents=[hr_agent, it_agent, generic_agent])
+
 if len(history) > 0:
     bot_history = [item['content'] for item in history if (item['role'] == 'assistant') and (item.get("name") is  None)]
     #trim history
@@ -89,11 +98,19 @@ if len(history) > 0:
                 message(user_history[i-1], is_user=True, key=str(i) + '_user') #this is because the bot starts first.
         message(bot_history[i], key=str(i))
 else:
-    history, agent_response = hr_agent.run(user_input=None)
+    history, agent_response = agent_runner.active_agent.init_history, agent_runner.active_agent.init_history[1]["content"]
     message(agent_response, is_user=False, key=str(0) + '_assistant')
     user_history=[]
 if user_input:
     message(user_input,  is_user=True,key=str(len(user_history)+1)+ '_user')
-    history, agent_response = hr_agent.run(user_input=user_input, conversation=history)
-    message(agent_response,  is_user=False,key=str(len(bot_history)+1)+ '_assistant')
+    previous_agent_last_response, history, agent_response = agent_runner.run(user_input=user_input, conversation=history)
+    agent_response_index = len(bot_history)+1
+    if previous_agent_last_response is not None:
+            message(previous_agent_last_response,  is_user=False,key=str(agent_response_index)+ '_assistant')
+            message(f"Agent {agent_runner.active_agent.name} is taking over the conversation.",  is_user=False,key=str(agent_response_index+1)+ '_assistant')
+            agent_response_index +=2
+    message(agent_response,  is_user=False,key=str(agent_response_index)+ '_assistant')
 st.session_state['history'] = history
+st.session_state['starting_agent_name'] = agent_runner.active_agent.name
+
+
--- scenarios/incubations/copilot/multi-agent/utils.py
+++ scenarios/incubations/copilot/multi-agent/utils.py
@@ -4,16 +4,17 @@ import openai
 import os
 from pathlib import Path  
 import json
-import time
+import re
 from dotenv import load_dotenv
 import concurrent.futures
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import inspect
-env_path = Path('.') / 'secrets.env'
+env_path = Path('..') / 'secrets.env'
 load_dotenv(dotenv_path=env_path)
 openai.api_key =  os.environ.get("AZURE_OPENAI_API_KEY")
 openai.api_base =  os.environ.get("AZURE_OPENAI_ENDPOINT")
 openai.api_type = "azure"
+
 class Search_Client():
     def __init__(self,emb_map_file_path):
         with open(emb_map_file_path) as file:
@@ -42,17 +43,12 @@ class Search_Client():
             text_content += f"{chunk_id}\n{content}\n"
         return text_content
 
-HR_PERSONA = """
-You are Lucy, an HR support specialist responsible for answering questions about HR & Payroll from employees and handling personal information updates.
-You start the conversation by validating the identity of the employee. Do not proceed until you have validated the identity of the employee.
-When you are asked with a question, use the search tool to find relavent knowlege articles to create the answer.
-Answer ONLY with the facts from the search tool. If there isn't enough information, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
-When employee request updating their address, use the tool provided to update in the system.
-For all other information update requests, log a ticket to the HR team to update the information.
-If the employee is asking for information that is not related to HR or Payroll, offer to route the call to general customer support then transfer the call.
-"""
-def search_knowlebase(search_query):
+search_client = Search_Client("../data/chunk_emb_map.json")
+ 
+def route_call(department):
+    return f"conversation is routed to {department}"
+
+def search_knowledgebase(search_query):
     return search_client.find_article(search_query)
 def validate_identity(employee_id):
     if employee_id in ["1234","5678"]:
@@ -64,14 +60,86 @@ def update_address(employee_id, new_address):
 def create_ticket(employee_id, updates):
     return f"A ticket number 1233445 has been created for employee {employee_id} with the following updates: {updates} "
 
+ROUTE_CALL_FUNCTION_NAME = "route_call" #default function name for routing call used by all agents
+GENERALIST_PERSONA = """
+You are Jenny, a helpful general assistant that can answer general questions about everything except HR and Payroll and IT.
+If the employee is asking for information in the HR & Payroll, offer to route the call to HR/Payroll specialist then transfer the call.
+If the employee is asking for information in the IT, offer to route the call to IT specialist then transfer the call.
+"""
+IT_PERSONA = """
+You are Paul, a helpful IT specialist that help employees about everything in IT.
+If the employee is asking for information that is not related to IT, offer to route the call to general customer support then transfer the call.
+"""
+
+HR_PERSONA = """
+You are Lucy, an HR support specialist responsible for answering questions about HR & Payroll from employees and handling personal information updates.
+You start the conversation by validating the identity of the employee. Do not proceed until you have validated the identity of the employee.
+When you are asked with a question, use the search tool to find relavent knowlege articles to create the answer.
+Answer ONLY with the facts from the search tool. If there isn't enough information, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
+Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+When employee request updating their address, use the tool provided to update in the system.
+For all other information update requests, log a ticket to the HR team to update the information.
+If the employee is asking for information that is not related to HR or Payroll, offer to route the call to general customer support then transfer the call.
+"""
+
 HR_AVAILABLE_FUNCTIONS = {
-            "search_knowledgebase": search_knowlebase,
+            "search_knowledgebase": search_knowledgebase,
             "validate_identity": validate_identity,
             "update_address": update_address,
             "create_ticket": create_ticket,
+            "route_call": route_call
 
         } 
-search_client = Search_Client("data/chunk_emb_map.json")
+IT_AVAILABLE_FUNCTIONS = {
+            "route_call": route_call
+
+        } 
+
+GENERAL_AVAILABLE_FUNCTIONS = {
+            "route_call": route_call
+
+        } 
+GENERAL_FUNCTIONS_SPEC= [  
+    {
+        "name": "route_call",
+        "description": "When the employee wants to talk about a topic that is not in IT, call this function to route the call to the appropriate department",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "department": {
+                    "type": "string",
+                    "description": "department to route the call to"
+                },
+
+            },
+            "required": ["department"],
+        },
+
+    },
+
+
+]  
+IT_FUNCTIONS_SPEC= [  
+    {
+        "name": "route_call",
+        "description": "When the employee wants to talk about a topic that is not in IT, call this function to route the call to the appropriate department",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "department": {
+                    "type": "string",
+                    "description": "department to route the call to"
+                },
+
+            },
+            "required": ["department"],
+        },
+
+    },
+
+
+]  
+
 
 HR_FUNCTIONS_SPEC= [  
     {
@@ -163,13 +231,57 @@ HR_FUNCTIONS_SPEC= [
 
 ]  
 
+
+class Agent_Runner():
+    def __init__(self,starting_agent_name, agents) -> None:
+        self.agents = agents
+
+        self.active_agent = None
+        for agent in agents:
+            if starting_agent_name == agent.name:
+                self.active_agent = agent
+                break
+        agent_descriptions = [(agent.name, agent.persona)for agent in self.agents]
+        text_description =""
+        for agent, description in agent_descriptions:
+            text_description += f"Name: {agent}, Job description: {description}\n\n"
+        
+        self.evaluator = Agent(engine="turbo-0613", persona="As a customer support manager, you have following agents with the description of their skills\n\n"+text_description)
+        
+    def revaluate_agent_assignment(self,conversation_history):
+        #TODO: revaluate agent assignment based on the state
+        names = [agent.name for agent in self.agents]
+        prompt =f"Review the last part of this conversation where there's a need to change the agent handling the call \n\{conversation_history[-4:]})\n\n. The most appropriate agent's name among {names} to take over the conversation is: "
+        next_agent = self.evaluator.generate_response(prompt).strip()
+        print("next agent ", next_agent)
+        for agent in self.agents:
+            if next_agent == agent.name:
+                self.active_agent = agent
+                print("agent changed to ", agent.name)
+                break
+            else:
+                print("agent not changed to ", agent.name)
+    def run(self,user_input, conversation=None, stream = False, api_version = "2023-07-01-preview"):
+        request_agent_change, conversation, assistant_response= self.active_agent.run(user_input, conversation=conversation, stream = False, api_version = api_version)
+        previous_agent_last_response=None
+        
+        if request_agent_change:
+            previous_agent_last_response = assistant_response
+            self.revaluate_agent_assignment(conversation)
+            conversation= self.active_agent.init_history
+            conversation.append({"role":"user", "content":user_input})
+            # conversation.append({"role":"assistant", "content":f"Agent {self.active_agent.name} is taking over the conversation."})
+            _,conversation, assistant_response= self.active_agent.run(conversation=conversation, stream = False, api_version = api_version)
+        return previous_agent_last_response, conversation, assistant_response
+        
+
 def gpt_stream_wrapper(response):
     for chunk in response:
         chunk_msg= chunk['choices'][0]['delta']
         chunk_msg= chunk_msg.get('content',"")
         yield chunk_msg
 class Agent(): #Base class for Agent
-    def __init__(self, engine, persona, init_message=None):
+    def __init__(self, engine,persona, name=None, init_message=None):
         if init_message is not None:
             init_hist =[{"role":"system", "content":persona}, {"role":"assistant", "content":init_message}]
         else:
@@ -178,6 +290,7 @@ class Agent(): #Base class for Agent
         self.init_history =  init_hist
         self.persona = persona
         self.engine = engine
+        self.name= name
     def generate_response(self, new_input,history=None, stream = False,request_timeout =20,api_version = "2023-05-15"):
         openai.api_version = api_version
         if new_input is None: # return init message 
@@ -243,18 +356,22 @@ class Smart_Agent(Agent):
     """
 
     def __init__(self, persona,functions_spec, functions_list, name=None, init_message=None, engine ="gpt-4"):
-        super().__init__(engine=engine,persona=persona, init_message=init_message)
+        super().__init__(engine=engine,persona=persona, name=name, init_message=init_message)
         self.functions_spec = functions_spec
         self.functions_list= functions_list
-    def run(self, user_input, conversation=None, stream = False, api_version = "2023-07-01-preview"):
+    def run(self, user_input=None, conversation=None, stream = False, api_version = "2023-07-01-preview"):
         openai.api_version = api_version
-        if user_input is None: #if no input return init message
-            return self.init_history, self.init_history[1]["content"]
+        request_agent_change = False
+        # if user_input is None: #if no input return init message
+        #     return self.init_history, self.init_history[1]["content"]
+        assistant_response=""
         if conversation is None: #if no history return init message
             conversation = self.init_history.copy()
-        conversation.append({"role": "user", "content": user_input})
+        if  user_input is not None:
+            conversation.append({"role": "user", "content": user_input})
         i=0
-        while True:
+        while True: # loop to retry in case there's an intermittent error from GPT
+
             try:
                 i+=1
                 response = openai.ChatCompletion.create(
@@ -262,11 +379,12 @@ class Smart_Agent(Agent):
                     messages=conversation,
                 functions=self.functions_spec,
                 function_call="auto", 
+                stream=stream,
+                request_timeout=15,
                 )
                 response_message = response["choices"][0]["message"]
 
-
-                    # Step 2: check if GPT wanted to call a function
+                # Step 2: check if GPT wanted to call a function
                 if  response_message.get("function_call"):
                     print("Recommended Function call:")
                     print(response_message.get("function_call"))
@@ -276,16 +394,19 @@ class Smart_Agent(Agent):
                     # Note: the JSON response may not always be valid; be sure to handle errors
                     
                     function_name = response_message["function_call"]["name"]
-                    
+                    if function_name == ROUTE_CALL_FUNCTION_NAME:
+                        request_agent_change = True
+                        
                     # verify function exists
                     if function_name not in self.functions_list:
-                        return "Function " + function_name + " does not exist"
+                        print("Function " + function_name + " does not exist")
+
                     function_to_call = self.functions_list[function_name]  
                     
                     # verify function has correct number of arguments
                     function_args = json.loads(response_message["function_call"]["arguments"])
                     if check_args(function_to_call, function_args) is False:
-                        return "Invalid number of arguments for function: " + function_name
+                        print("Invalid number of arguments for function: " + function_name)
                     function_response = function_to_call(**function_args)
                     print("Output of function call:")
                     print(function_response)
@@ -318,6 +439,7 @@ class Smart_Agent(Agent):
 
                     assistant_response = second_response['choices'][0]['message']["content"]
                     conversation.append({"role": "assistant", "content": assistant_response})
+
                 else:
                     assistant_response = response_message["content"]
                     conversation.append({"role": "assistant", "content": assistant_response})
@@ -326,6 +448,6 @@ class Smart_Agent(Agent):
                 if i>3: 
                     break
                 print("Exception as below, will retry\n", str(e))
-                time.sleep(5)
 
-        return conversation, assistant_response
+        return request_agent_change, conversation, assistant_response
+
