8d9a5976c6dca55a1b0e0ca556ea74901b3f9f47
james-tn <james.nguyen@microsoft.com>
2023-08-15 16:23:30 -0700

    add data management

--- scenarios/incubations/copilot/data_management/copilot.py
+++ scenarios/incubations/copilot/data_management/copilot.py
@@ -34,7 +34,10 @@ Depending on the needs of the user, the Copilot will assign the right agent to a
 
     - What's the forecast for government and product fan at December-01-2022?
     - Can you update the forecast for government and product fan to 3200 at December-01-2022?
-    If questions are not clear, copilot will ask for clarification.
+    
+                
+    If questions are not clear, copilot will ask for clarification. For example, if the request to update does not have product, business unit, copilot will ask for clarification. 
+                
     Behind the scene, Copilot uses function calling capability to query data and update forecast.
 3. Inventory forecast Copilot help query data and update forecast for inventory. It works similar to sales forecast Copilot.
 
