a06c598940c56228d80c761f79c681c44552742b
james-tn <james.nguyen@microsoft.com>
2023-09-13 10:33:14 -0700

    add tech expert

---
+++ scenarios/incubations/copilot/tech_expert/tech_copilot_utils.py
@@ -0,0 +1,63 @@
+# Agent class
+### responsbility definition: expertise, scope, conversation script, style 
+import openai
+import os
+from pathlib import Path  
+import json
+import time
+from azure.search.documents.models import Vector  
+import uuid
+from tenacity import retry, wait_random_exponential, stop_after_attempt  
+
+from dotenv import load_dotenv
+from azure.core.credentials import AzureKeyCredential  
+from azure.search.documents import SearchClient  
+from openai.embeddings_utils import get_embedding, cosine_similarity
+import inspect
+env_path = Path('..') / 'secrets.env'
+load_dotenv(dotenv_path=env_path)
+openai.api_key =  os.environ.get("AZURE_OPENAI_API_KEY")
+openai.api_base =  os.environ.get("AZURE_OPENAI_ENDPOINT")
+openai.api_type = "azure"
+import sys
+import random
+sys.path.append("..")
+from utils import Agent, Smart_Agent, check_args, search_knowledgebase
+
+
+    
+
+
+
+PERSONA = """
+You are Maya, an technical support specialist responsible for answering questions about computer science and system.
+When you are asked with a question, use the search tool to find relavent knowlege articles to create the answer.
+Answer ONLY with the facts from the search tool. If there isn't enough information, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
+Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+If the user is asking for information that is not related to computer science or computer system, say it's not your area of expertise.
+"""
+
+AVAILABLE_FUNCTIONS = {
+            "search_knowledgebase": search_knowledgebase,
+
+        } 
+
+FUNCTIONS_SPEC= [  
+    {
+        "name": "search_knowledgebase",
+        "description": "Searches the knowledge base for an answer to the technical question",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "search_query": {
+                    "type": "string",
+                    "description": "The search query to use to search the knowledge base"
+                }
+            },
+            "required": ["search_query"],
+        },
+    },
+
+]  
+
+
