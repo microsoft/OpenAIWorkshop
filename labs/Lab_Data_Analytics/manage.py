104525947e32fe8e907151d285e897b7a41c5203
kalalvishal <20169092+kalalvishal@users.noreply.github.com>
2023-10-03 07:13:18 -0700

    updating the labs based on the last workshop learnings.

--- labs/Lab_Data_Analytics/manage.py
+++ labs/Lab_Data_Analytics/manage.py
@@ -2,38 +2,69 @@ import os
 import sys
 import subprocess
 import venv
-
 import platform
 
-def check_python():
-    try:
-        result = subprocess.run(["python", "--version"], check=True, stdout=subprocess.PIPE)
-        python_version = result.stdout.decode().strip()
-        print("Python version: " + python_version)
-        if python_version != "Python 3.11.5":
-            print("Please download and install Python version 3.11.5 from the internet.")
-        else:
-            print("python Version 3.11.5 is installed and ready to use.")
-    except subprocess.CalledProcessError:
-        print("Python is not installed. Please download and install Python version 3.11.5 from the internet.")
-        # Add the steps to install Python here
+def check_python(osplatform):
+    if osplatform == 'windows':
+        try:
+            result = subprocess.run(["python", "--version"], check=True, stdout=subprocess.PIPE)
+            
+            python_version = result.stdout.decode().strip()
+            print("Python version: " + python_version)
+            
+            if python_version != "Python 3.11.5":
+                print("Please download and install Python version 3.11.5 from the internet.")
+                return False
+            else:
+                print("python Version 3.11.5 is installed and ready to use.")
+                return True
+            
+        except subprocess.CalledProcessError:
+            print("Python is not installed. Please download and install Python version 3.11.5 from the internet.")
+            # Add the steps to install Python here
+        return True
+    else:
+        try:
+            result = subprocess.run(["python3", "--version"], check=True, stdout=subprocess.PIPE)
+            python_version = result.stdout.decode().strip()
+            print("Python version: " + python_version)
+            if python_version != "Python 3.11.5":
+                print("Please download and install Python version 3.11.5 from the internet.")
+                return False
+            else:
+                print("python Version 3.11.5 is installed and ready to use.")
+        except subprocess.CalledProcessError:
+            print("Python is not installed. Please download and install Python version 3.11.5 from the internet.")
+            # Add the steps to install Python here
+    return True
 
-def check_pip():
-    try:
-        subprocess.run(["pip", "--version"], check=True)
-    except subprocess.CalledProcessError:
-        print("pip is not installed. Please download and install Python version 3.11.5 from the internet.")
-        # Add the steps to install pip here
-
+def check_pip(osplatform):
+    if osplatform == 'windows':
+        try:
+            subprocess.run(["pip", "--version"], check=True)
+            return True
+        except subprocess.CalledProcessError:
+            print("pip is not installed. Please download and install Python version 3.11.5 from the internet.")
+            # Add the steps to install pip here
+            return False
+    else:
+        try:
+            subprocess.run(["pip3", "--version"], check=True)
+            return True
+        except subprocess.CalledProcessError:
+            print("pip is not installed. Please download and install Python version 3.11.5 from the internet.")
+            # Add the steps to install pip here
+            return False
+        
 def manage_environment(command):
     env_dir = "aoaivenv"
-    check_python()
-    check_pip()
     
     osplatform = platform.system()
+    check_python(osplatform.lower())
+    check_pip(osplatform.lower())
     
     if osplatform.lower() == 'windows':
-        #print('windows activation')
+        print('windows activation')
         if command == "new":
             venv.create(env_dir, with_pip=True)
             activate_script = f".\\{env_dir}\\Scripts\\activate"
@@ -47,18 +78,18 @@ def manage_environment(command):
             deactivate_script = f".\\{env_dir}\\Scripts\\deactivate.bat"
             os.system(f"cmd /k {deactivate_script}")
     else:
-        #print('non-windows activation')
+        print('non-windows activation')
         if command == "new":
-            venv.create(env_dir, with_pip=True)
+            # venv.create(env_dir, with_pip=True)
+            os.system(f"python3 -m venv {env_dir}")
             activate_script = f".//{env_dir}//bin//activate"
-            os.system(f"cmd /k {activate_script}")
-            subprocess.run([f"{env_dir}/bin/python", "-m", "pip", "install", "-r", "requirements.txt"], check=True)
+            os.system(f"source {activate_script}  && pip3 install -r requirements.txt")
         elif command == "activate":
             activate_script = f".//{env_dir}//bin//activate"
-            os.system(f"cmd /k {activate_script}")
+            os.system(f"source {activate_script}")
         elif command == "deactivate":
             deactivate_script = f".//{env_dir}//bin//deactivate.bat"
-            os.system(f"cmd /k {deactivate_script}")
+            os.system(f"{deactivate_script}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or sys.argv[1] not in ["new", "activate", "deactivate"]:
